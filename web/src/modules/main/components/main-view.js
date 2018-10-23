91d97551c0d77afa663a5844c00dbc03a20cc8a7
Victor Moreno <victor.moreno@tomtom.com>
2018-10-23 11:22:00 +0200

    Third main commit: Create directories and modules

---
+++ web/src/modules/main/components/main-view.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './main-view.css';
+
+class MainView extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello World!</h1>
+            </div>
+        )
+    }
+}
+
+export default MainView;
